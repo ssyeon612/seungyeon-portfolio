557b377090aadd41d2e37fb4ac99f02c92c4437a
ssyeon612 <ssyeon612@gmai.com>
2023-11-02 17:04:03 +0900

    bug fix

--- src/components/DarkModeButton/index.tsx
+++ src/components/DarkModeButton/index.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 export default function DatrModeToggleButton() {
     const { theme, setTheme } = useTheme();
     return (
-        <>
+        <div>
             <input type="checkbox" id="toggle-mode-cb" checked={theme === "dark"} onChange={() => setTheme(theme === "dark" ? "light" : "dark")} />
             <div id="mode-wrapper" className="w-fit mb-10">
                 <label id="toggle-mode" htmlFor="toggle-mode-cb" className="cursor-pointer">
@@ -15,6 +15,6 @@ export default function DatrModeToggleButton() {
                     </span>
                 </label>
             </div>
-        </>
+        </div>
     );
 }
